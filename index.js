0068175940a98bbb4cb05a06890d11af19495c83
Geralyn Mendall <geralyn.mendall@gmail.com>
2020-01-14 11:32:53 -0500

    finished template engines and fixed errors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', '/views');
+app.set('views', './views'); // default
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
