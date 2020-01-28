27f718406af0bed81cb51c7023760b37c64e120d
Geralyn Mendall <geralyn.mendall@gmail.com>
2020-01-28 11:12:20 -0500

    created 'callback hell'

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,10 +1,24 @@
+//Asynchronous
 console.log('Before');
 getUser(1, (user) => {
     getRepositories(user.gitHubUsername, (repos) => {
-        console.log('Repos', repos);
+        getCommits(repo, (commits) => {
+            //CALLBACK HELL
+        });
     });
 });
 console.log('After');
+
+//Synchronous
+console.log('Before');
+const user = getUser(1);
+const repos = getRepositories(user.gitHubUsername);
+const commits =  getCommits(repos[0]);
+console.log('After');
+
+
+
+
 
 function getUser(id, callback) {
     setTimeout(() => {
